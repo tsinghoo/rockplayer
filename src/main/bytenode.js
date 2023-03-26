56cd8f8e4607979e0c6bafc83c06f6c89efad48b
tsinghoo <tsinghoo@hotmail.com>
2023-03-26 13:56:32 +0800

    update config

--- src/main/bytenode.js
+++ src/main/bytenode.js
@@ -7,18 +7,18 @@ const v8 = require('v8');
 
 v8.setFlagsFromString('--no-lazy');
 
-const pathName = path.join(__dirname, './main.js');
+const pathName = path.join(__dirname, './main');
 
 try {
     (async function () {
         try {
             await bytenode.compileFile({
-                filename: pathName,
+                filename: `${pathName}.js`,
                 electron: true,  // electron的项目这个参数一定要加上
                 compileAsModule: true
-            }, `${pathName}c`);
+            }, `${pathName}b.js`);
             // 将原来的js文件里面的内容替换成下面的内容
-            fs.writeFileSync(pathName, 'require("bytenode");require("./main.jsc");', 'utf8');
+            fs.writeFileSync(pathName, 'require("bytenode");require("./mainb.js");', 'utf8');
         } catch (e) {
             console.error(`run_bytenode_err: ${e}`);
         }
