68fae665d7a9d9029a11d368b707edb87aa91acf
liqinghu <liqinghu@91taogu.com>
2023-07-22 18:13:33 +0800

    tag

--- web/app.js
+++ web/app.js
@@ -149,7 +149,7 @@ app.get('/video/tag', (req, res) => {
     }
 
     fs.writeFileSync(path.join(directoryPath, "tags"), JSON.stringify(otags));
-    var resp = req.query.js + "(" + JSON.stringify(otags) + ");";
+    var resp = req.query.js + "(" + JSON.stringify({ data: otags }) + ");";
     //resp = JSON.stringify(otags);
     //res.jsonp(resp);
     res.send(resp);
