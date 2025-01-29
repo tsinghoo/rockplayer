7d503b708bc700cffa5d308e3f30486837323372
tsinghoo <tsinghoo@hotmail.com>
2025-01-29 11:47:16 +0000

    callStt

--- web/shell/callStt.sh
+++ web/shell/callStt.sh
@@ -1,10 +1,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-
+# in whisper env: 
+# pip install rapid_videocr
+# apt-get install libgl1
 stt(){
   today=`date +%Y-%m-%d`
   python ./stt.py  >> ./stt.$today.log 2>&1 
 }
+
+stt
 
 toexit=0
 while [ "$toexit" == "0" ];do
