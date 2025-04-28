a244a64c61900148a8d06f966f3536e97937d516
liqinghu <liqinghu@91taogu.com>
2025-04-28 14:59:06 +0800

    sz

--- web/app.js
+++ web/app.js
@@ -1083,7 +1083,7 @@ function formatScode(stockCode) {
     if (code.length == 6) {
         if (/^(600|601|603|605|688|900)\d+$/.test(code)) {
             suffix = "SH"; // 上交所（600/601/603/605/688/900 开头）
-        } else if (/^(000|001|002|003|30)\d+$/.test(code)) {
+        } else if (/^(000|001|002|003|30|15)\d+$/.test(code)) {
             suffix = "SZ"; // 深交所（000/001/002/003/300 开头）
         } else if (/^(8|43|83|87|88)\d+$/.test(code)) {
             suffix = "BJ"; // 北交所（8/43/83/87/88 开头）
