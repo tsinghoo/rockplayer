4424af2fe829071856ed8754e6026adc85664c80
liqinghu <liqinghu@91taogu.com>
2025-05-23 11:23:00 +0800

    fix json parse

--- web/public/video/stock.js
+++ web/public/video/stock.js
@@ -352,13 +352,14 @@ window.feed_list = window.feed_list || (function () {
                         td.html(html);
                         td.addClass("ruleContent");
                     } else if (key == "状态") {
-                        let rc = {};
+                        let rc = row[key];
                         try {
-                            rc = JSON.parse(row[key]);
+                            rc = JSON.parse(rc);
+                            rc = JSON.stringify(rc, null, 2)
                         } catch (e) {
 
                         }
-                        let html = `<pre>${JSON.stringify(rc, null, 2)}</pre>`;
+                        let html = `<pre>${rc}</pre>`;
                         td.html(html);
                         td.addClass("ruleStatus");
                         td.removeClass("nowrap");
