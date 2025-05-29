52ac0e51664800227d9b73718f75a8a75c5a9b4b
liqinghu <liqinghu@91taogu.com>
2025-05-29 10:04:36 +0800

    920

--- web/app.js
+++ web/app.js
@@ -1515,7 +1515,7 @@ function formatScode(stockCode) {
             suffix = "SH"; // 上交所（600/601/603/605/688/900 开头）
         } else if (/^(000|001|002|003|30|15|12|3)\d+$/.test(code)) {
             suffix = "SZ"; // 深交所（000/001/002/003/300 开头）
-        } else if (/^(8|43|83|87|88)\d+$/.test(code)) {
+        } else if (/^(8|43|83|87|88|920)\d+$/.test(code)) {
             suffix = "BJ"; // 北交所（8/43/83/87/88 开头）
         }
     } else if (/^\d{4,5}$/.test(code) || /^0[0-9]\d{3}$/.test(code)) {
