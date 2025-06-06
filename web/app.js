e7d1746085b8628e31930496b3c6ca38a44ccf80
liqinghu <liqinghu@91taogu.com>
2025-06-06 14:29:53 +0800

    syncPositions

--- web/app.js
+++ web/app.js
@@ -1010,7 +1010,7 @@ async function upgradeDb(succ, fail) {
         "update config set value='29' where key='dbVersion';",
         `create table t5m(id text primary key, scode text, time text, open real, close real, high real, low real, volume int, amount real);`,
         "update config set value='31' where key='dbVersion';",
-        `alter table tposition add column floatProfit real default 0;`,
+        `alter table tpositions add column floatProfit real default 0;`,
         "update config set value='33' where key='dbVersion';",
         
     ];
