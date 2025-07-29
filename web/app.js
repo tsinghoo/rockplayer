e4bd43ba8d382369334424aad90f8075dfdfd23b
tsinghoo <tsinghoo@gmail.com>
2025-07-29 12:00:54 +0800

    fix bug

--- web/app.js
+++ web/app.js
@@ -1618,7 +1618,7 @@ app.post('/stock/details', async (req, res) => {
     if (data.length < 1) {
         await db.runSync(`UPDATE tstock 
                     SET sname = (SELECT tsb.sname FROM tStockBasic tsb WHERE tsb.scode = tstock.scode)
-                    WHERE EXISTS (SELECT 1 FROM tStockBasic tsb WHERE tsb.scode = tstock.scode);`);
+                    WHERE EXISTS (SELECT 1 FROM tStockBasic tsb WHERE tsb.scode = tstock.scode);`, []);
     }
 
     res.send("ok");
