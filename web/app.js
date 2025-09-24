f8d82e83551ce9879204f0b72d18bb0aaba60b8d
liqinghu <liqinghu@91taogu.com>
2025-09-24 10:19:17 +0800

    pair latest

--- web/app.js
+++ web/app.js
@@ -2339,7 +2339,7 @@ app.get('/stock/pair', async (req, res) => {
     for (var i = 0; i < sells.length; ++i) {
         let sell = sells[i];
         info(`${sell.sname}(${sell.scode}):${sell.tid}`, req)
-        let r = await db.allSync("select * from tstock where tamount=? and scode=? and operationName=? and tprice<? and (tpair='' or tpair is null) order by tday, ttime, tprice desc",
+        let r = await db.allSync("select * from tstock where tamount=? and scode=? and operationName=? and tprice<? and (tpair='' or tpair is null) order by tday desc, ttime desc, tprice desc",
             [sell.tamount * -1, sell.scode, sell.operationName, sell.tprice]);
         let buys = r.rows;
         if (buys.length > 0) {
