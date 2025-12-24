ca9f2110dd6155fbd7225bbef63c77461765d8c1
liqinghu <liqinghu@91taogu.com>
2025-12-24 19:02:08 +0800

    formatScode

--- web/public/video/stock.js
+++ web/public/video/stock.js
@@ -1577,7 +1577,7 @@ window.stock_list = window.stock_list || (function () {
             if (code.length == 6) {
                 if (/^(600|601|603|605|688|900|51|58|56)\d+$/.test(code)) {
                     suffix = "SH"; // 上交所（600/601/603/605/688/900 开头）
-                } else if (/^(000|001|002|003|30|15)\d+$/.test(code)) {
+                } else if (/^(000|001|002|003|30|15|16|12|3)\d+$/.test(code)) {
                     suffix = "SZ"; // 深交所（000/001/002/003/300 开头）
                 } else if (/^(8|43|83|87|88|92)\d+$/.test(code)) {
                     suffix = "BJ"; // 北交所（8/43/83/87/88 开头）
