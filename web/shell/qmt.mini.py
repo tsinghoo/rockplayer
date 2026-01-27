71cb4a2315c041367ee6e664abccad4512ea9299
tsinghoo <tsinghoo@gmail.com>
2026-01-27 20:32:19 +0800

    opt k/upload

--- web/shell/qmt.mini.py
+++ web/shell/qmt.mini.py
@@ -905,28 +905,31 @@ def update1d(stocklist=None, startTime=None, endTime=None):
                     info(
                         f"newData={newData}, idx={idx}, foundStart={foundStart}, dataStartTime={dataStartTime}"
                     )
-
-            body = {
-                "data": obj2Json(batch_data),
-                "scode": scode,
-                "period": period,
-                "passcode": "995560",
-            }
-            debug("body:", body)
-            # 上传数据到test1
-            try:
-                response = requests.post(
-                    g.baseUrl + "/stock/k/upload", json=body, verify=False, timeout=20
-                )
-                if response.status_code != 200:
-                    error(
-                        "上传失败，状态码:",
-                        response.status_code,
-                        "响应内容:",
-                        response.text,
+            if len(batch_data) > 0:
+                body = {
+                    "data": obj2Json(batch_data),
+                    "scode": scode,
+                    "period": period,
+                    "passcode": "995560",
+                }
+                debug("body:", body)
+                # 上传数据
+                try:
+                    response = requests.post(
+                        g.baseUrl + "/stock/k/upload",
+                        json=body,
+                        verify=False,
+                        timeout=20,
                     )
-            except Exception as e:
-                error("上传失败:", str(e))
+                    if response.status_code != 200:
+                        error(
+                            "上传失败，状态码:",
+                            response.status_code,
+                            "响应内容:",
+                            response.text,
+                        )
+                except Exception as e:
+                    error("上传失败:", str(e))
 
 
 def KDJ(table):
@@ -1123,7 +1126,7 @@ def update1m(stocklist, startTime=None):
                     "passcode": "995560",
                 }
                 debug("body:", body)
-                # 上传数据到test1
+                # 上传数据
                 try:
                     response = requests.post(
                         g.baseUrl + "/stock/k/upload",
