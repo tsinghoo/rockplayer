999e8b7c854ffd850d05b7899ba1b233005861ee
tsinghoo <tsinghoo@gmail.com>
2026-03-19 11:34:44 +0800

    recalc

--- web/app.js
+++ web/app.js
@@ -3661,7 +3661,7 @@ async function genCci(scode, req, all) {
         error(`${r.rows.length} < ${period} 1d data`, req.threadId)
         return;
     }
-    if ((r.rows[0].cci == -200 || r.rows[1].cci == -200) && !all) {
+    if ((r.rows[0].cci == -200 && r.rows[1].cci == -200) && !all) {
         info(`need recalc all cci`, req.threadId);
         genCci(scode, req, 1);
         return;
