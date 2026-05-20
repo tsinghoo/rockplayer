c98a7a8a0c714abcffb961e7bc753a2173e191d3
tsinghoo <tsinghoo@gmail.com>
2026-05-20 20:44:44 +0800

    info

--- web/shell/sv.sh
+++ web/shell/sv.sh
@@ -1,5 +1,5 @@
 ps -ef|grep "app.js"|grep "node" |awk '{print $2}'|xargs kill -9 
 
 cd ~/git/rockplayer/web/ 
-nohup nodemon app.js 3001 /flv '.webm\;.mp4\;.mp3\;.m4a\;.ts\;.avi\;.pdf\;.docx\;.pptx' &
+nohup nodemon app.js 3001 /flv '.webm\;.mp4\;.mp3\;.m4a\;.ts\;.avi\;.pdf\;.docx\;.pptx' info &
 ps -ef|grep "app.js"|grep "node" |awk '{print $2}'
