71758fae17b602178e3eec6091443b5d3912eaa6
tsinghoo <tsinghoo@gmail.com>
2026-06-02 18:47:39 +0800

    start.wine

---
+++ web/shell/qmt.mini.start.wine.cmd
@@ -0,0 +1,17 @@
+@echo off
+cd /d "%~dp0"
+:begin
+cls
+@echo start
+
+set PATH=z:\data\soft\pythonwin3.6.8\;%PATH%
+@echo PATH=%PATH%
+set startGjzqqmt=0
+set env=1
+set url=4
+
+
+python .\qmt.mini.start.py 
+
+@echo end
+pause
